274d0e68ded831c272742d98a3acf371d3ce23ca
filioelio <filio_cs_23@hotmail.com>
2022-02-09 14:49:54 -0500

    Convolucion completo

--- Js/Base.js
+++ Js/Base.js
@@ -1,6 +1,6 @@
 var image = new Image();
 image.onload = imageLoaded;
-image.src = "./Img/IMG_5960.jpg"
+image.src = "./Img/IMG_0355.jpg"
 
 function imageLoaded(){
     var canvas = document.getElementById("canvas");
@@ -12,6 +12,9 @@ function imageLoaded(){
     ctx.drawImage(image, 0, 0, image.width, image.height);
 
     WriterAndBlack(canvas);
+
+    var result = document.getElementById("result");
+    convolucionar(canvas, result);
 }
 
 function WriterAndBlack(canvas) {
@@ -33,4 +36,57 @@ function WriterAndBlack(canvas) {
 
     ctx.putImageData(imgData, 0, 0)
     // console.log(imgData);
+}
+
+function convolucionar(canvas,  result){
+    //obtener todas las variables
+    var ctxCanvas = canvas.getContext('2d');
+    var imageCanvas = ctxCanvas.getImageData(0,0, canvas.width, canvas.height);
+    var pixelesCanvas = imageCanvas.data;
+
+    result.width = canvas.width;
+    result.height = canvas.height
+    var ctxResult = result.getContext('2d');
+    var imageResult = ctxResult.getImageData(0,0, result.width, result.height);
+    var pixelesResult = imageResult.data;
+
+    //nucleo, kernel => soel
+    var kernel = [
+        [-1,-1,-1],
+        [-1,8,-1],
+        [-1,-1,-1],
+    ];
+
+    for(var y = 1 ; y < canvas.height-1; y++) {
+        for(var x = 1 ; x < canvas.width-1; x++) {
+            //posición en el arreglo js
+            var idx =((y*canvas.width) + x) *4
+            //casilla
+            var casilla1 = kernel[0][0]  * pixelesCanvas[((((y-1)*canvas.width) + (x-1)) *4)]
+            var casilla2 = kernel[0][1]  * pixelesCanvas[((((y-1)*canvas.width) + (x)) *4)]
+            var casilla3 = kernel[0][2]  * pixelesCanvas[((((y-1)*canvas.width) + (x+1)) *4)]
+            var casilla4 = kernel[1][0]  * pixelesCanvas[((((y)*canvas.width) + (x-1)) *4)]
+            var casilla5 = kernel[1][1]  * pixelesCanvas[((((y)*canvas.width) + (x)) *4)]
+            var casilla6 = kernel[1][2]  * pixelesCanvas[((((y)*canvas.width) + (x+1)) *4)]
+            var casilla7 = kernel[2][0]  * pixelesCanvas[((((y+1)*canvas.width) + (x-1)) *4)]
+            var casilla8 = kernel[2][1]  * pixelesCanvas[((((y+1)*canvas.width) + (x)) *4)]
+            var casilla9 = kernel[2][2]  * pixelesCanvas[((((y+1)*canvas.width) + (x+1)) *4)]
+
+            var resultEnd = casilla1 + casilla2 + casilla3 + casilla4 + casilla5+casilla6 + casilla7 + casilla8 + casilla9;
+
+            pixelesResult[idx] = resultEnd ; // red
+            pixelesResult[idx+1] = resultEnd; // green  
+            pixelesResult[idx+2] = resultEnd;// blue
+            pixelesResult[idx+3] = 255;// alpha
+
+            //convierte todo la imagen en gris
+            // pixelesResult[idx] = pixelesCanvas[idx] ; // red
+            // pixelesResult[idx+1] = pixelesCanvas[idx+1]; // green  
+            // pixelesResult[idx+2] = pixelesCanvas[idx+2];// blue
+            // pixelesResult[idx+3] = pixelesCanvas[idx+3];// alpha
+        }
+    }
+    ctxResult.putImageData(imageResult, 0, 0)
+
+
 }
